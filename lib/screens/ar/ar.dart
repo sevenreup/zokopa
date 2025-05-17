893049d8cadf5c84521020d0c1117a2e44cb8e3f
Christopher Seven Phiri <chris7phiri@gmail.com>
2025-05-17 10:24:13 +0200

    save

--- lib/screens/ar/ar.dart
+++ lib/screens/ar/ar.dart
@@ -19,7 +19,7 @@ class _DebugOptionsWidgetState extends State<DebugOptionsWidget> {
   bool _showPlanes = false;
   bool _showWorldOrigin = false;
   bool _showAnimatedGuide = true;
-  String _planeTexturePath = "Images/triangle.png";
+  String _planeTexturePath = "assets/images/logo.png";
   bool _handleTaps = false;
 
   @override
